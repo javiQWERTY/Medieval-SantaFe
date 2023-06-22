cd6e2a5dc8dc1cfc1856fb1b7fd3f642ea1e6c37
javiQWERTY <jamdev98@gmail.com>
2023-06-22 20:59:04 -0300

    Nuevo Repositorio.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, BaseTexture, SCALE_MODES, Sprite, settings } from 'pixi.js'
+
+settings.ROUND_PIXELS = true;
+BaseTexture.defaultOptions.scaleMode = SCALE_MODES.NEAREST;
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -8,6 +11,36 @@ const app = new Application({
 	width: 640,
 	height: 480
 });
+
+window.addEventListener("resize", () => {
+	console.log("resized");
+	//Escala.
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX, scaleY);
+
+	//Cambiar el tamaño de lo que se esta renderizando.
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+
+	if(app.view.style){
+		app.view.style.width = gameWidth + "px";
+		app.view.style.height = gameHeight + "px";
+	}
+	
+	//Calculo del margen.
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);
+
+	//Centrar el "juego".
+	if(app.view.style){
+		(app.view.style as any).marginLeft = marginHorizontal + "px";
+		(app.view.style as any).marginRight = marginHorizontal + "px";
+		(app.view.style as any).marginTop = marginVertical + "px";
+		(app.view.style as any).marginBottom = marginVertical + "px";
+	}
+})
+window.dispatchEvent(new Event("resize"));
 
 const clampy: Sprite = Sprite.from("clampy.png");
 
